f5dbb4c83fc17aa6ecae96630f84b867787d1fef
chamod udara <uchamod52@gmail.com>
2024-12-24 15:28:12 +0530

    Deploy site

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
-const isGitHubPages = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production";
 
-const repositoryName = "Portfolio-web-site";
 
-import type { NextConfig } from "next";
+// const repositoryName = "Portfolio-web-site";
 
-const nextConfig: NextConfig = {
+// import type { NextConfig } from "next";
+
+module.exports = {
   // /* config options here */
   output: "export", // Required to generate a static export
-  basePath: isGitHubPages ? `/${repositoryName}` : "",
+
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
+  basePath: "/Portfolio-web-site",
+  assetPrefix: isProd ? "/<your-repository-name>/" : "",
   // basePath: "/Portfolio-web-site",
   // assetPrefix: "/Portfolio-web-site",
   // trailingSlash: true, // Optional: Add trailing slashes to paths
@@ -19,4 +22,4 @@ const nextConfig: NextConfig = {
 //   output: "next export",
 //   // basePath: '/Portfolio-web-site',
 // };
-export default nextConfig;
+// export default nextConfig;
