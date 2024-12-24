c95fedf5bd3f982e9eb7c0e6e839bdc5a1926547
chamod udara <uchamod52@gmail.com>
2024-12-24 14:34:20 +0530

    add new site

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
+
+const isGitHubPages = process.env.NODE_ENV === 'production';
+
+const repositoryName = 'your-repository-name'; 
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  swcMinify: true,
+  // /* config options here */
+  output: "export", // Required to generate a static export
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  // basePath: "/Portfolio-web-site",
+  // assetPrefix: "/Portfolio-web-site",
+  // trailingSlash: true, // Optional: Add trailing slashes to paths
 };
 // module.exports = {
-//   output: 'export',
-//  // basePath: '/Portfolio-web-site',
+//   output: "next export",
+//   // basePath: '/Portfolio-web-site',
 // };
 export default nextConfig;
